a4cce949f417fd2915a6650a6d50e845a0491b9a
mohammad-adil <shahaadil13@gmail.com>
2021-07-04 23:28:54 +0530

    updating task

--- src/models/user.js
+++ src/models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 
-const User = mongoose.model('User', {
+const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
@@ -39,5 +39,14 @@ const User = mongoose.model('User', {
         }
     }
 })
+
+userSchema.pre('save', async function (next) {
+    const user = this
+
+    next()
+})
+
+
+const User = mongoose.model('User', userSchema)
 
 module.exports = User
